07906f08b9e4c791013d310f4a0ad631dcd2407b
alrdelcr <125524310+alrdelcr@users.noreply.github.com>
2023-02-21 15:22:30 -0800

    mla aspect is now working

--- bot.py
+++ bot.py
@@ -7,13 +7,14 @@ import ratemyprofessor
 import json
 import os.path
 from Messages import help_message
+import newspaper
+from newspaper import Article
 
 
 
-botDescription="Bot"
 intents = discord.Intents.default()
 intents.message_content = True
-client = commands.Bot(command_prefix="?", help_command=None, description=botDescription, intents=intents)
+client = commands.Bot(command_prefix="?", help_command=None, description="Bot", intents=intents)
 
 
 
@@ -45,11 +46,11 @@ async def on_message(message):
             await message.channel.send(r)
             await message.channel.send(d)
             if ratings.rating <= 2:
-                await message.channel.send("Now me personally... I would wait for next quarter")
+                await message.channel.send("I would wait for next quarter...")
             elif ratings.rating < 4 and ratings.rating > 2:
                 await message.channel.send("They're alright, I would take the chance if you really need it")
             else:
-                await message.channel.send("Beautiful...take the class")
+                await message.channel.send("Professor is great, TAKE THE CLASS")
                 return
         except:
             await message.channel.send("I have no idea who that is; Try Again?")
@@ -82,10 +83,10 @@ async def on_message(message):
             if len(l) == 0:
                 await message.channel.send("All tasks completed.")
         elif (message.content[0:3].lower()) == "add":
+            message.content = message.content[3::].strip()
             with open('tasks','r') as f:
                 l = json.load(f)
-            t = (message.content.split(' ')[1])
-            l.append(t)
+            l.append(message.content)
             with open('tasks','w') as f:
                     json.dump(l, f)
         else: # input is tasks that you want completed that day: !td ...,....,...
@@ -96,12 +97,39 @@ async def on_message(message):
             await message.channel.send("Tasks specified!")
             return
 
+    elif message.content.startswith('!mla'): #cite source in mla
+        try:
+            d = {'01':'Jan.','02':'Feb.','03':'Mar.','04':'Apr.','05':'May','06':'June','07':'July','08':'Aug.','09':'Sep.','10':'Oct.','11':'Nov.','12':'Dec.'}
+            url = str(message.content[4::].strip())
+            article = Article(url,language="en")
+            article.download()
+            article.parse()
+            title = article.title
+            publishdate = str(article.publish_date)
+            authors = article.authors
+            if publishdate == "None":
+                await message.channel.send("Publish date needed, please input (Format: year-month-day, ex: 2003-10-30): ")
+                message = await client.wait_for("message")
+                publishdate = (str(message.content)).split("-")
+            else:
+                publishdate = (publishdate.split()[0]).split("-")
+            if len(authors) == 0:
+                await message.channel.send("author needed, please input(Format: FirstName LastName): ")
+                message = await client.wait_for("message")
+                authors = [message.content]
+            if title == None:
+                await message.channel.send("title of article needed, please input: ")
+                message = await client.wait_for("message")
+                title = str(message.content)
+            publishdate[1] = d[publishdate[1]]
+            publishdate = publishdate[2] + ", " + publishdate[1] + " " + publishdate[0]
+            authors = (authors[0].split())[::-1]
+            mag = url.split(".")[1]
+            mag = mag[0].upper() + mag[1::]
+            citation = ", ".join(authors) + ". " + "\"" + title + "\"" + ". " + mag + ", "+ publishdate + ", " + url
+            await message.channel.send(citation)
+            return
+        except:
+            await message.channel.send("Sorry, mla citation not applicable for given source, would you like to do it manually?(yes/no)")
 
-    
-    elif message.content.startswith('!mat'): #math help
-        return
-    elif message.content.startswith('!mla'): #cite source
-        return
-    elif message.content.startswith('!play'): #music
-        return
 client.run(parser.get("token","token"))
