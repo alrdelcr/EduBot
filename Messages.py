3c44ae048488e51b1befbb10e8dd0658bad588e5
alrdelcr <125524310+alrdelcr@users.noreply.github.com>
2023-03-01 08:23:52 -0800

    Adding help messsage

---
+++ Messages.py
@@ -0,0 +1,29 @@
+
+
+help_message = """
+The available commands are:
+
+!rmp: (FORMAT: !rmp school name, professor name)
+\treturns to the user the specified professor's rmp info
+
+!td: allows the user to create a checklist of must do tasks
+    tasks (FORMAT: !td task1, task2, task3, ...)
+    \tSaves tasks to be done
+
+    add task (FORMAT: !td add task1)
+    \tAdds task to to-do list
+
+    finished task (FORMAT: !td finished task1)
+    \tRemoves task from to-do list
+        
+    List of Tasks (FORMAT: !td tasks)
+    \tPrints your tasks
+
+!mla: Allows the user to cite an online source given the url of article.
+    cite (FORMAT: !mla url)
+    \tcites the given source, prompts the user to input missing information
+    \talso has a manual input feature
+
+"""
+
+
